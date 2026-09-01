0e001a08442041f84a1d8c682bb58652c6b87917
Norton2005 <tolipovhusniddin2005@gmail.com>
2026-09-01 21:04:16 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -3,42 +3,43 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 const http = require("http");
-const fs = require("fs");
 
-let user;
-fs.readFile("database/user.json","utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data)
-    }
-});
+// let user;
+// fs.readFile("database/user.json","utf8", (err, data) => {
+//     if(err) {
+//         console.log("ERROR:", err);
+//     } else {
+//         user = JSON.parse(data)
+//     }
+// });
 
 // 1: Kirish code
-app.use(express.static("public")); 
+app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 // 2: session code
-// 3: Views code 
+// 3: Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
 app.post("create-item", (req, res) => {
-    //TODO: code with db here
+  //TODO: code with db here
 });
 
-app.get("/author", (req, res) => {
-    res.render("author", { user: user } );
-})
+// app.get("/author", (req, res) => {
+//     res.render("author", { user: user } );
+// })
 
 app.get("/", function (req, res) {
-    res.render("harid");
+  res.render("reja");
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-    console.log(`The server is running successfully on port: ${PORT}`);
+  console.log(
+    `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`,
+  );
 });
