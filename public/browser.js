1ec9524044f18bda1a55edf2d1c5b046e9c697b5
Norton2005 <tolipovhusniddin2005@gmail.com>
2026-09-08 19:07:48 +0900

    feat: Reja loyihamizni yakuniy bosqich logic

--- public/browser.js
+++ public/browser.js
@@ -49,6 +49,25 @@ document.addEventListener("click", function (e) {
 
   //edit oper
   if (e.target.classList.contains("edit-me")) {
-    alert("Siz edit buttonni bosdingiz");
+    let userInput = prompt("Ozgartirish kiriting", e.target.parentElelment.parentElelment.querySelector(".item-text").innerHTML
+);
+if (userInput) {
+    axios.post("/edit-item", {id: e.target.getAttribute("data-id"), new_input: userInput,
+    })
+    .then((response) => {
+        console.log(response.data);
+        e.target.parentElelment.parentElelment.querySelector(".item-text").innerHTML = userInput;
+    })
+    .catch((err) => {
+        console.log("Iltimosqaytadan harakat qiling!");
+    }); 
+}
   }
+});
+
+document.getElementById("clean-all").addEventListener("click", function () {
+    axios.post("/delete-all", { delete_all: true }).then((response) => {
+        alert(response.data.state);
+        document.location.reload();
+    });
 });
